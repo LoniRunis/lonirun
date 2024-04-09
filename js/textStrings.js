6a05a6ce17383f8e8783625eaad7486e1cae8a33
Loni Runiswara <135214830+LoniRunis@users.noreply.github.com>
2024-04-09 11:20:04 -0700

    Add files via upload

--- js/textStrings.js
+++ js/textStrings.js
@@ -3,145 +3,209 @@
 ///////////////////////////////////
 
 var textStrings_EN = {
-	// Act 1
-	"niceHat": "OOH TOPI YANG BAGUS!",
-	"outtaHere": "pergilah dari sini",
-	"getARoom": "EWW, JANGAN DI TEMPAT UMUM",
-	"notCoolAnymore": "Topi tidak keren lagi",
-	"tvOnTv": "TV DI DALAM TV!",
-	"cricky": "LIL' CRICKY <3",
-	"tooManyCrickets": "okay itu terlalu banyak jangkrik",
-	"normalPeep": "hanya hal biasa",
-	"normalPeeps": "hanya hal-hal biasa",
-	"wowNothing": "WOWWEE, TIDAK ADA APA APA",
+    // Act 1
+    "niceHat"              : "OOH NICE HAT"                                          ,
+    "outtaHere"            : "yeah git on outta here"                                ,
+    "getARoom"             : "GROSS, GO GET A ROOM"                                  ,
+    "notCoolAnymore"       : "nvm hats aren't cool anymore"                          ,
+    "tvOnTv"               : "A TV... ON TV!"                                        ,
+    "cricky"               : "LIL' CRICKY <3"                                        ,
+    "tooManyCrickets"      : "okay that's too many crickets"                         ,
+    "normalPeep"           : "just a normal peep"                                    ,
+    "normalPeeps"          : "just some normal peeps"                                ,
+    "wowNothing"           : "WOWWEE, IT'S NOTHING"                                  ,
+    
+    // Act 2
+    "crazySquareAttacks"   : "CRAZED SQUARE ATTACKS"                                 ,
+    "justMissed"           : "oooooh just missed it"                                 ,
+    "somethingInteresting" : "(ya gotta catch 'em doing *something* interesting...)" ,
+    "whoIsScreaming"       : "(ya gotta catch who's screaming at 'em)"               ,
+    "circleFearsSquares"   : "CIRCLE FEARS SQUARES"                                  ,
+    "whoScaresThem"        : "(ya gotta also catch *who* they're scared by)"         ,
+    "areTheyScared"        : "(ya gotta catch 'em *being* scared by a square)"       , // not used
+    "squaresSnubCircles"   : "SQUARES SNUB CIRCLES"                                  ,
+    "areTheySnubbed"       : "(ya gotta catch 'em *while* snubbing a circle)"        , // not used
+    "everyoneHates"        : "EVERYONE HATES EVERYONE!!1!"                           ,
+    "almostEveryoneHates"  : "ALMOST EVERYONE HATES EVERYONE..."                     ,
+    "squaresHateCircles"   : "SQUARES HATE CIRCLES"                                  ,
+    "circlesHateSquares"   : "CIRCLES HATE SQUARES"                                  ,
+    "areTheyYelling"       : "(ya gotta catch 'em *yelling* at others)"              , // not used
+    "nerdsNow"             : "what are these nerds doing now"                        ,
+    "schockedPeep"         : "why's this peep shocked?"                              ,
+    "whatever"             : "whatever"                                              ,
+    
+    // Act 3
+    "ellipsis"             : ". . ."                                                 ,
+    "coolNoMore"           : "hats ain't cool no more, haven't you heard?"           ,
+    "beScared"             : "BE SCARED. BE ANGRY."                                  ,
+    
+    // MANIFESTO
+    "manifesto" : [
+       //"as if you viewers want GOOD news"       ,
+       "who tunes in to watch *people get along?*",
+       "peace is boring. violence goes viral."    ,
+       //"peace is boring. conflict gets clicks." ,
+       "and every story needs a conflict, so..."  ,
+       //"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
+       "GIVE THE AUDIENCE WHAT THEY WANT."
+    ] ,
+    
+    // -- Strings currently in graphics resources --
+    
+    // (bg_preload.png) Preload screen
+    "playingTime"        : "playing time: 5 minutes" ,
+    "warning"            : "warning: the following program\ncontains scenes of snobbery,\nrudeness & mass murder.\nviewer discretion is advised." ,
+    
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"      : "POINT & CLICK" ,
+    
+    // (chyron3.png)
+    "chyronNothing"      : "WOWWEE, IT'S *NOTHING*" ,
+    
+    // (credits0001.png)
+    "createdBy"          : "created by" ,
+    "NickyCase"          : "Nicky Case" ,
+    
+    // (credits0002.png)
+    "manyThanks"         : "many thanks to my playtesters:" ,
+    
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"  : "and to my patreon supporters:" ,
+    
+    // (credits0007.png)
+    "lastButNotLeast"    : "and last but not least," ,
+    
+    // (credits0008.png)
+    "thankYouForPlaying" : "thank YOU for playing!" ,
+    
+    // (preload_play.png)
+    "playButton"         : "PLAY" ,
+    
+    // (end_button.png) Post Credits
+    "otherWorkButton"    : "see my other work" ,
+    "buyCoffeeButton"    : "buy me a coffee" ,
+    "replayButton"       : "replay this mess" ,
+    
+    // (logo.png) Post Credits
+    "logoWBWWB"          : "SHARE THE PAIN:" ,
+    
+    // (end_prototype.png)
+    "endOfPrototype"     : "END OF PROTOTYPE" ,
+    "toBeContinued"      : "(to be continued!)" ,
+    
+    // (quote0002.png)
+    "WBWWB"              : "We become what we behold." ,
+    "WSOTATOTSU"         : "We shape our tools and then our tools shape us." ,
+    
+    // (quote0003.png)
+    "MarshallMcLuhan"    : "Marshall McLuhan" ,
+    
+    // (quote0004.png)
+    "misatrributed"      : "(misattributed)" ,
+};
 
-	// Act 2
-	"crazySquareAttacks": "SERANGAN PERSEGI YANG GILA",
-	"justMissed": "oooooh melewatkannya",
-	"somethingInteresting": "(kamu harus menangkap *sesuatu* yang menarik...)",
-	"whoIsScreaming": "(kamu harus memfoto siapa yang berteriak)",
-	"circleFearsSquares": "LINGKARAN TAKUT AKAN PERSEGI",
-	"whoScaresThem": "(kamu juga harus memfoto *apa* yang membuat mereka takut)",
-	"areTheyScared": "(kamu harus memfoto *mereka* takut akan persegi)", // not used
-	"squaresSnubCircles": "PERSEGI MENGHINA LINGKARAN",
-	"areTheySnubbed": "(kamu harus memfoto *mereka* saling menghina)", // not used
-	"everyoneHates": "SEMUANYA SALING MEMBENCI!!1!",
-	"almostEveryoneHates": "HAMPIR SEMUA SALING MEMBENCI...",
-	"squaresHateCircles": "PERSEGI MEMBENCI LINGKARAN",
-	"circlesHateSquares": "LINGKARAN MEMBENCI PERSEGI",
-	"areTheyYelling": "(kamu harus memfoto mereka saling berteriak)", // not used
-	"nerdsNow": "apa yang orang aneh ini lakukan",
-	"schockedPeep": "kenapa dia kaget?",
-	"whatever": "whatever",
+// Umlaut codes: Ä = \xC4, Ö = \xD6, Ü = \xDC, ä = \xE4, ö = \xF6, ü = \xFC, ß = \x
 
-<<<<<<< HEAD
 var textStrings_DE = {
     // Act 1
-    ///////////////////////////////////
-///// TEXT STRINGS ////////////////
-///////////////////////////////////
-
-var textStrings_EN = {
-	// Act 1
-	"niceHat": "OOH TOPI YANG BAGUS!",
-	"outtaHere": "pergilah dari sini",
-	"getARoom": "EWW, JANGAN DI TEMPAT UMUM",
-	"notCoolAnymore": "Topi tidak keren lagi",
-	"tvOnTv": "TV DI DALAM TV!",
-	"cricky": "LIL' CRICKY <3",
-	"tooManyCrickets": "okay itu terlalu banyak jangkrik",
-	"normalPeep": "hanya hal biasa",
-	"normalPeeps": "hanya hal-hal biasa",
-	"wowNothing": "WOWWEE, TIDAK ADA APA APA",
-
-	// Act 2
-	"crazySquareAttacks": "SERANGAN PERSEGI YANG GILA",
-	"justMissed": "oooooh melewatkannya",
-	"somethingInteresting": "(kamu harus menangkap *sesuatu* yang menarik...)",
-	"whoIsScreaming": "(kamu harus memfoto siapa yang berteriak)",
-	"circleFearsSquares": "LINGKARAN TAKUT AKAN PERSEGI",
-	"whoScaresThem": "(kamu juga harus memfoto *apa* yang membuat mereka takut)",
-	"areTheyScared": "(kamu harus memfoto *mereka* takut akan persegi)", // not used
-	"squaresSnubCircles": "PERSEGI MENGHINA LINGKARAN",
-	"areTheySnubbed": "(kamu harus memfoto *mereka* saling menghina)", // not used
-	"everyoneHates": "SEMUANYA SALING MEMBENCI!!1!",
-	"almostEveryoneHates": "HAMPIR SEMUA SALING MEMBENCI...",
-	"squaresHateCircles": "PERSEGI MEMBENCI LINGKARAN",
-	"circlesHateSquares": "LINGKARAN MEMBENCI PERSEGI",
-	"areTheyYelling": "(kamu harus memfoto mereka saling berteriak)", // not used
-	"nerdsNow": "apa yang orang aneh ini lakukan",
-	"schockedPeep": "kenapa dia kaget?",
-	"whatever": "whatever",
-
-=======
->>>>>>> 13a2e8affb99b826288f940175cf2bbf40ff0086
-	// Act 3
-	"ellipsis": ". . .",
-	"coolNoMore": "hats ain't cool no more, haven't you heard?",
-	"beScared": "TAKUTLAH, MARAHLAH.",
-
-	// MANIFESTO
-	"manifesto": [
-		//"as if you viewers want GOOD news"       ,
-		"siapa yang menonton untuk melihat orang orang rukun*",
-		"perdamaian membosankan. kekerasan menjadi viral",
-		//"peace is boring. conflict gets clicks." ,
-		"dan setiap cerita butuh konflik,jadi..",
-		//"...GIVE THE AUDIENCE WHAT THEY WANT."   ,
-		"BERIKAN APA YANG MEREKA INGINKAN PADA PENONTON."
-	],
-
-	// -- Strings currently in graphics resources --
-
-	// (bg_preload.png) Preload screen
-	"playingTime": "playing time: 5 minutes",
-	"warning": "PERINGATAN: Program ini\nmengandung adegan kekerasan,\nkerusuhan.\nkebijaksanaan penonton disarankan.",
-
-	// (cam-instructions.png) Camera Instructions
-	"pointAndClick": "POINT & CLICK",
-
-	// (chyron3.png)
-	"chyronNothing": "WOWWEE, IT'S *NOTHING*",
-
-	// (credits0001.png)
-<<<<<<< HEAD
-	"createdBy": "Code game dari",
-	"NickyCase": "Nicky case",
-
-	// (credits0002.png)
-	"manyThanks": "moded By:",
-=======
-	"createdBy": "modded by",
-	"NickyCase": "Loni Runiswara",
-
-	// (credits0002.png)
-	"manyThanks": "Tujuan game ini:",
->>>>>>> 13a2e8affb99b826288f940175cf2bbf40ff0086
-
-	// (credits0007.png)
-	"lastButNotLeast": "and last but not least,",
-
-	// (credits0008.png)
-	"thankYouForPlaying": "thank YOU for playing!",
-
-	// (preload_play.png)
-	"playButton": "PLAY",
-
-
-
-	// (end_prototype.png)
-	"endOfPrototype": "END OF PROTOTYPE",
-	"toBeContinued": "(to be continued!)",
-
-	// (quote0002.png)
-	"WBWWB": "We become what we behold.",
-	"WSOTATOTSU": "We shape our tools and then our tools shape us.",
-
-	// (quote0003.png)
-	"MarshallMcLuhan": "Marshall McLuhan",
-
-	// (quote0004.png)
-	"misatrributed": "(misattributed)",
+    "niceHat"              : "Oh, h\xFCbscher Hut!"                                              ,
+    "outtaHere"            : "Ja, besser schnell weg hier."                                      ,
+    "getARoom"             : "Widerlich, nehmt euch ein Zimmer!"                                 ,
+    "notCoolAnymore"       : "Schon geh\xF6rt? H\xFCte sind out."                                ,
+    "tvOnTv"               : "Ein Fernseher! Im Fernsehen!"                                      ,
+    "cricky"               : "Ein Grash\xFCpfer!"                                                ,
+    "tooManyCrickets"      : "Ok, zu viele Grash\xFCpfer."                                       ,
+    "normalPeep"           : "Ein gew\xF6hnlicher Typ."                                          ,
+    "normalPeeps"          : "Ein paar gew\xF6hnliche Typen."                                    ,
+    "wowNothing"           : "Wow! Nichts! Gar nix!"                                             ,
+    
+    // Act 2
+    "crazySquareAttacks"   : "Verr\xFCcktes Quadrat greift an!"                                  ,
+    "justMissed"           : "Ooooh, knapp verpasst."                                            ,
+    "somethingInteresting" : "(laaangweilig *g\xE4hn*)"                                          ,
+    "whoIsScreaming"       : "(*wer* schreit sie denn an?)"                                      ,
+    "circleFearsSquares"   : "KREIS F\xDCRCHTET QUADRATE!"                                       ,
+    "whoScaresThem"        : "(*wen* f\xFCrchten sie denn?)"                                     ,
+    "areTheyScared"        : "(erwische sie, *wenn* sie fl\xFCchten)"                            ,
+    "squaresSnubCircles"   : "QUADRAT: Anschuldigungen von KREISEN l\xE4cherlich."               ,
+    "areTheySnubbed"       : "(erwische sie, *w\xE4hrend* sie angreifen)"                        ,
+    "everyoneHates"        : "ALLE HASSEN JEDEN!!1!"                                             ,
+    "almostEveryoneHates"  : "FAST JEDER HASST ALLE..."                                          ,
+    "squaresHateCircles"   : "QUADRATE HASSEN KREISE"                                            ,
+    "circlesHateSquares"   : "KREISE HASSEN QUADRATE"                                            ,
+    "areTheyYelling"       : "(erwische sie, *w\xE4hrend* sie sich gegenseitig anschreien)"      ,
+    "nerdsNow"             : "Was machen diese Nerds jetzt?"                                     ,
+    "schockedPeep"         : "Warum ist er geschockt?"                                           ,
+    "whatever"             : "Egal..."                                                           ,
+    
+    // Act 3
+    "ellipsis"             : ". . ."                                                             ,
+    "coolNoMore"           : "H\xFCte sind out. Hast du's immer noch nicht geh\xF6rt?"           ,
+    "beScared"             : "HABT ANGST. SEID W\xDCTEND."                                        ,
+    
+    // MANIFESTO
+    "manifesto" : [
+       //"Als ob ihr an guten Nachrichten interessiert seid."     ,
+       "Wer will denn sehen, wie Leute miteinender klarkommen?" ,
+       "Frieden ist langweilig. Die Leute wollen Gewalt sehen." ,
+       //"Frieden ist langweilig. Konflikt bringt Klicks."        ,
+       "Jede gute Story braucht einen Konflikt, also..."        ,
+       //"...GIB DEM PUBLIKUM WAS ES WILL!"                       ,
+       "GIB DEM PUBLIKUM WAS ES WILL!"
+    ] ,
+    
+    // -- Strings currently in graphics resources --
+    
+    // (bg_preload.png) Preload screen
+    "playingTime"        : "Spielzeit: 5 Minuten" ,
+    "warning"            : "Warnung: die folgende Sendung\nenth\xE4lt Darstellungen von Arroganz,\nBeleidigungen & Gewalt.\nund ist nicht f\xFCr Kinder geeignet." ,
+    
+    // (cam-instructions.png) Camera Instructions
+    "pointAndClick"      : "POINT & CLICK" , // I'd leave that in English, even for the German translation. /stefan
+    
+    // (chyron3.png)
+    "chyronNothing"      : "Oh wow, *NICHTS*!" ,
+    
+    // (credits0001.png)
+    "createdBy"          : "" , // I'd leave that in English, even for the German translation. /stefan
+    "NickyCase"          : "" , // name; just here in case you want to display text instead of graphic. /stefan
+    
+    // (credits0002.png)
+    "manyThanks"         : "Vielen Dank an alle Tester:" ,
+    
+    // (credits000[3,4,5,6].png)
+    "patreonSupporters"  : "und an meine Patreon Unterst\xFCtzer:" ,
+    
+    // (credits0007.png)
+    "lastButNotLeast"    : "und zu guter Letzt," ,
+    
+    // (credits0008.png)
+    "thankYouForPlaying" : "Danke an DICH f\xFCr's spielen!" ,
+    
+    // (preload_play.png)
+    "playButton"         : "START" ,
+    
+    // (end_button.png) Post Credits
+    "otherWorkButton"    : "andere Projekte" ,
+    "buyCoffeeButton"    : "kauf mir einen Kaffee" ,
+    "replayButton"       : "noch mal spielen" ,
+    
+    // (logo.png) Post Credits
+    "logoWBWWB"          : "TEILE DEINEN SCHMERZ:" ,
+    
+    // (end_prototype.png)
+    "endOfPrototype"     : "ENDE DES PROTOTYPS" ,
+    "toBeContinued"      : "(wird fortgesetzt!)" ,
+    
+    // (quote0002.png)
+    "WBWWB"              : "Wir werden, was wir sehen." , // I'd leave that in English, even for the German translation. /stefan
+    "WSOTATOTSU"         : "Wir formen unsere Werkzeuge, und dann formen unsere Werkzeuge uns." ,
+    
+    // (quote0003.png)
+    "MarshallMcLuhan"    : "Marshall McLuhan" , // name; just here in case you want to display text instead of graphic. /stefan
+    
+    // (quote0004.png)
+    "misatrributed"      : "(falsch zugeschrieben)" ,
 };
 
 var textStrings = textStrings_DE;
